8352d3db3fd07ba325822d116d42b5925a65ff6e
William Bajzek <william@gurustugroup.com>
2012-10-25 13:12:29 -0500

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # Create Wordpress Uploads Folder symlink
 # New Project
+git remote rm origin
+git remote rm gitlab
 projectname=`pwd | awk 'BEGIN {FS="/"} {print $NF}'`
 mkdir /var/www/wp-uploads/$projectname
 chmod 777 /var/www/wp-uploads/$projectname
@@ -7,6 +9,6 @@ chmod 777 /var/www/wp-uploads/$projectname
 # Create link
 ln -s /var/www/wp-uploads/$projectname wp/wp-content/uploads
 
-sed -i "s/Theme Name: Cleanslate/Theme Name: $projectname/" wp/wp-content/themes/cleanslate/style.css > wp/wp-content/themes/cleanslate/style.css
-mv wp/wp-content/themes/cleanslate wp/wp-content/themes/$projectname
+sed -i "s/Theme Name: Cleanslate/Theme Name: $projectname/" wp/wp-content/themes/cleanslate/style.css
+git mv wp/wp-content/themes/cleanslate wp/wp-content/themes/$projectname
 
