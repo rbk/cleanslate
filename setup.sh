d1ba916706d27049e4378183a0433c0bef1918e1
William Bajzek <william@gurustugroup.com>
2012-12-18 15:02:57 -0600

    fixed some problems with setup script due to wordpress upgrade

--- setup.sh
+++ setup.sh
@@ -7,24 +7,28 @@ touch .htaccess && chmod 777 .htaccess
 git remote rm origin
 projectname=`pwd | awk 'BEGIN {FS="/"} {print $NF}'`
 username=`whoami`
+
+echo $username
+
+
+cp wp/wp-config-sample.php wp/wp-config.php
+sed -i "s/database_name_here/$projectname/" wp/wp-config.php
+sed -i "s/your_project_name/$projectname/" wp/wp-config.php
+sed -i "s/username_here/root/" wp/wp-config.php
+sed -i "s/your_user_name/$username/" wp/wp-config.php
+sed -i "s/password_here/GuRuStu1881/" wp/wp-config.php
+
+
+# Create link
+[ ! -d /var/www/wp-uploads/$projectname ] && mkdir /var/www/wp-uploads/$projectname
+[ ! -d /var/www/wp-uploads/$projectname ] && ln -s /var/www/wp-uploads/$projectname wp/wp-content/uploads
+
+
 git remote add origin git@localhost:$projectname.git
 git remote add upstream git@localhost:cleanslate.git
-
-
-
 
 [ ! -d wp/wp-content/themes/$projectname ] && git mv wp/wp-content/themes/cleanslate wp/wp-content/themes/$projectname
 sed -i "s/Theme Name: Cleanslate/Theme Name: $projectname/" wp/wp-content/themes/$projectname/style.css
 
-cp wp/wp-config-sample.php wp/wp-config.php
-sed -i "s/your_project_name/$projectname/" wp/wp-config.php
-sed -i "s/your_user_name/$username/" wp/wp-config.php
-sed -i "s/username_here/root/" wp/wp-config.php
-sed -i "s/password_here/GuRuStu1881/" wp/wp-config.php
-
-[ ! -d /var/www/wp-uploads/$projectname ] && mkdir /var/www/wp-uploads/$projectname
-
 chmod 777 /var/www/wp-uploads/$projectname
 
-# Create link
-[ ! -d /var/www/wp-uploads/$projectname ] && ln -s /var/www/wp-uploads/$projectname wp/wp-content/uploads
