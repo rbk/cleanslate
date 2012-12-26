d23cfc0dc7faf70fe36e878aecf19e4fe2ddded3
William Bajzek <william@gurustugroup.com>
2012-12-26 13:33:14 -0600

    include Guru.etc javascript in admin section

--- wp/wp-content/themes/cleanslate-neue/functions.php
+++ wp/wp-content/themes/cleanslate-neue/functions.php
@@ -5,6 +5,22 @@ function cleanslate_neue_setup() {
 	wp_enqueue_script('jquery','https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js');
 	wp_enqueue_script('plugins-scripts',get_stylesheet_directory_uri() . '/js/plugins.js');
 	wp_enqueue_script('common-scripts',get_stylesheet_directory_uri() . '/js/common.js');
+
+	// as long as this comment is here, the following probably hasn't been tested yet...
+	// but it's based on this codex doc:
+	// http://codex.wordpress.org/Plugin_API/Action_Reference/after_setup_theme
+
+	// First we check to see if our default theme settings have been applied.
+	$the_theme_status = get_option( 'theme_setup_status' );
+	if ( $the_theme_status !== '1' ) {
+		// delete first post, page, and comment
+		wp_delete_post( 1, true );
+		wp_delete_post( 2, true );
+		wp_delete_comment( 1 );
+
+		// Once done, we register our setting to make sure we don't duplicate everytime we activate.
+		update_option( 'theme_setup_status', '1' );
+	}
 }
 
 add_action( 'after_setup_theme', 'cleanslate_neue_setup' );
@@ -24,6 +40,7 @@ function setup_guru_js() {
 	<?php
 }
 add_action('wp_head', 'setup_guru_js',1);
+add_action('admin_head', 'setup_guru_js',1);
 
 
 
