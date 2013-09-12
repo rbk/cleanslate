6a4e9a38293b19291ae54d7c603478a0202b19a3
Richard Keller <richard@gurustugroup.com>
2013-09-12 13:58:13 -0500

    small change to template

--- wp/wp-content/themes/cleanslate-clean/page.php
+++ wp/wp-content/themes/cleanslate-clean/page.php
@@ -11,13 +11,27 @@
 		<article class="post" id="post-<?php the_ID(); ?>">
 
 			<h2><?php the_title(); ?></h2>
-
 			<div class="entry">
+				<?php
+					if( has_post_thumbnail() ){														
+						$thumbID = get_post_thumbnail_id($post->ID);
+						$thumbTitle = get_the_title( $thumbID );
+						$thumbSrc = wp_get_attachment_image_src( $thumbID, 'page-thumb' );
+						$banner = array(
+						'url' => $thumbSrc[0],
+						'width' => $thumbSrc[1],
+						'height' => $thumbSrc[2]
+
+						);
+
+						echo '<div class="pic">';
+						echo '<img src="'.$banner['url'].'" width="'.$banner['width'].'" height="'.$banner['height'].'" />';
+						// echo '<div><span>'.$thumbTitle.'</span></div>';
+						echo '</div>';
+					}
+				?>
 
 				<?php the_content(); ?>
-
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-
 			</div>
 
 			<?php edit_post_link('Edit this page.', '<p>', '</p>'); ?>
