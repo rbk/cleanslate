175883d2b6516856b2b5c011fa12485c23b22fdf
Richard Keller <richard@gurustugroup.com>
2013-11-12 00:09:15 -0600

    added foundation

--- wp/wp-content/themes/cleanslate-clean/config.rb
+++ wp/wp-content/themes/cleanslate-clean/config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 
-# require "zurb-foundation"
+require "zurb-foundation"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
