211deb1c1f6bba6ebc5db1231e50deead636f091
Richard Keller <richard@gurustugroup.com>
2013-11-11 22:20:10 -0600

    commiting deleted files, removed password form filter

--- wp/wp-content/themes/cleanslate-clean/functions.php
+++ wp/wp-content/themes/cleanslate-clean/functions.php
@@ -138,30 +138,6 @@
 	}
 	/*
 	 *
-	 * Custom Password Form for protected pages
-	 *
-	*/
-	add_filter( 'the_password_form', 'custom_password_form' );  
-	function custom_password_form() {  
-		global $post;  
-		$label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID ); 
-		$o = '<form class="protected-post-form" action="' . get_option('siteurl') . '/wp-login.php?action=postpass" method="post"> 
-		' . __( "" ) . ' 
-		
-		<label class="pass-label" for="' . $label . '">' . __( "PASSWORD:" ) . ' </label>
-		
-		<input name="post_password" id="' . $label . '" type="password" style="background: #ffffff; border:1px solid #999; color:#333333; padding:10px;" size="20" />
-
-		<input type="submit" name="Submit" class="button" value="' . esc_attr__( "Submit" ) . '" />  
-		
-		</form>
-		<p style="font-size:14px;margin:0px;"></p>  
-		';
-
-		return $o;  
-	}
-	/*
-	 *
 	 * Add editor style
 	 *
 	*/
